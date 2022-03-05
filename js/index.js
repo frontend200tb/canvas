c0883c0e9a5b0b1472945cf54bdf8d078a4ff3ed
Иван Каширин <85587225+frontend200tb@users.noreply.github.com>
2022-03-06 00:14:53 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,66 @@
+console.log('frontend200tb touch slider');
+
+
+/*****************
+Константы
+*****************/
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
+
+
+/*****************
+Переменные
+*****************/
+context.strokeStyle = "#020c29";
+
+/*****************
+Фигуры
+*****************/
+/* Окружность 
+arc(x, y, radius, startAngle, endAngle, anticlockwise)*/
+context.beginPath();
+context.arc(100, 100, 50, 0, Math.PI*2, false);
+context.closePath();
+context.stroke();
+
+
+
+/* Треугольник 
+moveTo(x, y)
+lineTo(x, y)
+*/
+context.beginPath();
+context.moveTo(600, 50);
+context.lineTo(700, 50);
+context.lineTo(650, 150);
+context.lineTo(600, 50);
+context.stroke();
+
+/* Квадрат 
+rect(x, y, width, height)*/
+context.beginPath();
+context.rect(50, 450, 100, 90);
+context.closePath();
+context.stroke();
+
+/* Прямоугольник 
+rect(x, y, width, height)
+*/
+context.beginPath();
+context.rect(550, 450, 200, 90);
+context.closePath();
+context.stroke();
+
+
+
+/*****************
+Функции
+*****************/
+
+
+
+/*****************
+События
+*****************/
+
+console.log('frontend200tb memory finish');
